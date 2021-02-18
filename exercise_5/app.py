178b1fde25a9813a7fd2a69eb8ceeacfa61210d6
Frederick Zhu <zhu.yuming@outlook.com>
2021-02-18 02:15:11 -0600

    Finished exercise_5

--- exercise_5/app.py
+++ exercise_5/app.py
@@ -1,37 +1,31 @@
 import string
+import json
 import random
-from datetime import datetime
-from flask import Flask, render_template, request
+from threading import Thread
+from datetime import datetime, timedelta
+from flask import Flask, request, redirect
 from functools import wraps
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# sample_chats = {
-#     1: {
-#         "authorized_users": {
-#             "session_token_0": {"username": "Alice", "expires": "2020-02-15T20:53:15Z"},
-#             "session_token_1": {"username": "Bob", "expires": "2020-02-15T20:57:22Z"}
-#         },
-#         "magic_key": "some_really_long_key_value"
-#         "messages": [
-#             {"username": "Alice", "body": "Hi Bob!"},
-#             {"username": "Bob", "body": "Hi Alice!"},
-#             {"username": "Alice", "body": "Knock knock"},
-#             {"username": "Bob", "body": "Who's there?"},
-#         ]
-#     }
-# }
+ROOM_CAPACITY = 6
+TOKEN_LENGTH = 8
+KEY_LENGTH = 16
 chats = {}
+tokens = []
+
+# token_monitor = Thread(target=monitoring_tokens, daemon=True)
+# token_monitor.start()
 
 def newChat(host, session_token):
     authorized_users = dict([
         (session_token, dict([
             ("username", host),
-            ("expires", datetime.utcnow() + timedelta(hours=6))
+            ("expires", get_str_expires())
         ]))
     ])
-    magic_key = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+    magic_key = gen_key()
 
     return dict([
         ("authorized_users", authorized_users),
@@ -39,6 +33,41 @@ def newChat(host, session_token):
         ("messages", [])
     ])
 
+# def monitoring_tokens():
+#     global tokens
+#     while True:
+#         if len(tokens) > 0 and tokens[0]
+
+def is_valid_auth(request):
+    chat_id = request.args.get("chat_id", type=int)
+    magic_key = request.args.get("magic_key")
+    return (chat_id != None and magic_key != None and 
+                chats.get(chat_id) != None and
+                magic_key == chats[chat_id]["magic_key"])
+
+def is_valid_session(chat_id, request):
+    session_token = request.args.get("session_token")
+    return (session_token != None and chats.get(chat_id) != None and
+                session_token in chats[chat_id]["authorized_users"].keys())
+
+def get_str_expires():
+    expires = datetime.utcnow() + timedelta(hours=6)
+    return expires.isoformat(timespec='seconds') + 'Z'
+
+def get_str_timestamp():
+    return datetime.utcnow().isoformat(timespec='seconds') + 'Z'
+
+def gen_key():
+    return ''.join(random.choices(string.ascii_lowercase+string.digits, 
+                                    k=KEY_LENGTH))
+
+def gen_token():
+    return ''.join(random.choices(string.ascii_lowercase+string.digits, 
+                                    k=TOKEN_LENGTH))
+
+
+
+# -------------------------------- WEB ROUTES ----------------------------------
 
 @app.route('/')
 def index(chat_id=None):
@@ -46,41 +75,204 @@ def index(chat_id=None):
 
 @app.route('/auth')
 def auth():
-    return app.send_static_file('auth.html')
+    if is_valid_auth(request):
+        return app.send_static_file('auth.html')
+    return "Invalid magic invite link."
 
 @app.route('/chat/<int:chat_id>')
-def chat():
-    return app.send_static_file('chat.html')
+def chat(chat_id):
+    if (chat_id != None and is_valid_session(chat_id, request)):
+        return app.send_static_file('chat.html')
+    return redirect('/')
+    
 
 
 # -------------------------------- API ROUTES ----------------------------------
 
 @app.route('/api/create', methods=['POST'])
-def create ():
-    # TODO: generate a session token for the user who created the chat
-    # TODO: create a chat data structure in global memory
-
+def create():
+    req = request.get_json()
+    token = gen_token()
+    chat = newChat(req['host'], token)
+    chat_id = 1
+    while True:
+        if (chats.get(chat_id) == None or 
+                len(chats[chat_id]['authorized_users']) == 0):
+            break
+        chat_id += 1
+    chats[chat_id] = chat
     return {
-        "chat_id": 1,
-        "session_token": "some_token_value",
-        "magic_invite_link": "http://localhost:5000/auth?chat_id=1&magic_key=some_really_long_key_value"
+        "chat_id": chat_id,
+        "session_token": token,
+        "magic_invite_link": 
+            "http://localhost:5000/auth?chat_id={}&magic_key={}"
+            .format(chat_id, chat['magic_key'])
     }
 
 @app.route('/api/authenticate', methods=['POST'])
 def authenticate():
-    # TODO: check if the request body contains a chat_id and the correct magic_key for that chat
-    # TODO: also send a username
-    return {"session_token": "some_token_value"}
+    if not is_valid_auth(request):
+        return {
+            "success": False,
+            "message": "Invalid magic invite link.",
+            "token": None
+        }
 
-@app.route('/api/messages', methods=['GET', 'POST'])
-def messages ():
-    # TODO: check if the request body contains a chat_id and valid session token for that chat
-    if request.method == 'POST':
-        # TODO: add the message
-        pass
+    req = request.get_json()
+    chat_id = req['chat_id']
+    username = req['user']
+    users = chats[chat_id]['authorized_users']
+    print(users)
+    for session_token, info in users.items():
+        if info['username'] == username:
+            info['expires'] = get_str_expires()
+            return {
+                "success": True,
+                "message": "User already authenticated.",
+                "session_token": session_token
+            }
+    if len(users) == ROOM_CAPACITY:
+        return {
+            "success": False,
+            "message": "Max room capacity reached.",
+            "session_token": None
+        }
 
-    messages = {} # TODO: get the messages for this chat from global memory
+    new_session_token = gen_token()
+    users[new_session_token] = {
+        "username": username,
+        "expires": get_str_expires()
+    }
     return {
-        "messages": messages,
-        "magic_invite_link": "http://localhost:5000/chat/1?magic_key=some_really_long_key_value"
+        "success": True,
+        "message": "User authenticated; session token generated.",
+        "session_token": new_session_token
+    }
+
+@app.route('/api/chat', methods=['GET'])
+def get_chat():
+    chat_id = request.args.get("chat_id", type=int)
+    is_valid_request = (chat_id != None and chats.get(chat_id) != None and 
+                            is_valid_session(chat_id, request))
+
+    if not is_valid_request:
+        return {
+            "success": False,
+            "chat_id": None,
+            "chat": None
+        }
+    return {
+        "success": True,
+        "chat_id": chat_id,
+        "chat_info": chats[chat_id]
+    }
+
+@app.route('/api/link', methods=['GET'])
+def get_link():
+    chat_id = request.args.get("chat_id", type=int)
+    is_valid_request = (chat_id != None and chats.get(chat_id) != None and 
+                            is_valid_session(chat_id, request))
+
+    if not is_valid_request:
+        return {
+            "success": False,
+            "magic_link": None,
+        }
+    
+    magic_key = chats[chat_id]['magic_key']
+    magic_link = ("http://localhost:5000/auth?chat_id={}&magic_key={}"
+                    .format(chat_id, magic_key))
+    return {
+        "success": True,
+        "magic_link": magic_link,
+    }
+
+@app.route('/api/chats', methods=['GET'])
+def get_chats():
+    chat_id = request.args.get("chat_id", type=int)
+    session_token = request.args.get("session_token")
+    username = ""
+    is_valid = True
+    if chat_id != None:
+        if is_valid_session(chat_id, request):
+            username = chats[chat_id]['authorized_users'][session_token]['username']
+        else:
+            is_valid = False
+    else:
+        for chat_id, chat in chats.items():
+            if session_token in chat['authorized_users'].keys():
+                username = chat['authorized_users'][session_token]['username']
+                break;
+        if username == "":
+            is_valid = False
+    if is_valid == False:
+        return {
+            "success": False,
+            "username": None,
+            "chats": None
+        }
+    user_chats = []
+    for chat_id, chat in chats.items():
+        for token, user in chat['authorized_users'].items():
+            if username == user['username']:
+                user_chats.append({
+                    "chat_id": chat_id,
+                    "session_token": token
+                })
+    return {
+        "success": True,
+        "username": username,
+        "chats": user_chats,
+    }
+
+@app.route('/api/messages', methods=['GET'])
+def get_messages():
+    # TODO: check if the request body contains a chat_id and valid session token for that chat
+    chat_id = request.args.get("chat_id", type=int)
+    is_valid_request = (chat_id != None and chats.get(chat_id) != None and 
+                            is_valid_session(chat_id, request))
+
+    if not is_valid_request:
+        return {
+            "success": False,
+            "chat_id": None,
+            "messages": None,
+        }
+    return {
+        "success": True,
+        "chat_id": chat_id,
+        "messages": chats[chat_id]['messages']
+    }
+
+@app.route('/api/messages', methods=['POST'])
+def post_message():
+    # TODO: check if the request body contains a chat_id and valid session token for that chat
+    chat_id = request.args.get("chat_id", type=int)
+    is_valid_request = (chat_id != None and chats.get(chat_id) != None and 
+                            is_valid_session(chat_id, request))
+    if not is_valid_request:
+        return {
+            'success': False,
+            'message': "Invalid chat session."
+        }
+
+    req = request.get_json()
+    timestamp = get_str_timestamp()
+    chat_id = req['chat_id']
+    session_token = req['session_token']
+    message_body = req['message_body']
+    chat = chats[chat_id]
+    username = chat['authorized_users'][session_token]['username']
+    messages = chat['messages']
+    messages.append({
+        "username": username,
+        "body": message_body,
+        "timestamp": timestamp
+    })
+    while (len(messages) > 30):
+        messages.pop(0)
+
+    return {
+            'success': True,
+            'message': "Message sent to chat room."
     }
--- exercise_5/static/script.js
+++ exercise_5/static/script.js
@@ -1,34 +1,197 @@
-/* For index.html */
+const MSG_ROLLING_INTERVAL = 500
 
-// TODO: If a user click to create a chat, create a session token for them
-// and save it. Redirect the user to /chat/<chat_id>
+function createGetRequest(api) {
+  return fetch(api, {
+    method: 'GET',
+    headers: new Headers({
+      "content-type": "application/json"
+    })
+  })
+}
+
+function createPostRequest(api, strJSON) {
+  return fetch(api, {
+    method: 'POST',
+    body: strJSON,
+    headers: new Headers({
+      "content-type": "application/json"
+    })
+  })
+}
+
+function showUsernameErr() {
+  if (document.getElementById("username-error").textContent == ""){
+    let usernameErrMsg = document.createTextNode("Please create your username.");
+    document.getElementById("username-error").appendChild(usernameErrMsg);
+  }
+}
+
 function createChat() {
+  let username = document.getElementById("username").value;
+  if (username == "") {
+    showUsernameErr()
+    return;
+  }
 
+  let api = '/api/create';
+  let body = JSON.stringify({ host: username });
+  let request = createPostRequest(api, body)
+
+  request.then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+      window.location.replace(`/chat/${data.chat_id}?session_token=${data.session_token}`);
+    })
+  })
 }
 
 /* For auth.html */
-
-// TODO: On page load, pull chat_id and magic_key out of the URL parameters
-// Send them to the auth API endpoint to get a session token
-// If the user authenticaes successfully, save the session token
-// and redirect them to /chat/<chat_id>
 function authenticate() {
-  return;
+  const urlParams = new URLSearchParams(window.location.search);
+  let chatId = parseInt(urlParams.get('chat_id'))
+  let magicKey = urlParams.get('magic_key')
+  let username = document.getElementById("username").value;
+
+  if (username == "") {
+    showUsernameErr()
+    return;
+  }
+
+  let api = `/api/authenticate?chat_id=${chatId}&magic_key=${magicKey}`;
+  let body = JSON.stringify({
+    chat_id: chatId,
+    magic_key: magicKey,
+    user: username,
+  });
+  let request = createPostRequest(api, body)
+
+  request.then(function(response) {
+    response.json().then(function(data) {
+      console.log(data.success)
+      if (data.success == true) {
+        window.location.replace(`/chat/${chatId}?session_token=${data.session_token}`);
+      } else {
+        window.location.replace('/');
+      }
+    })
+  })
 }
 
 /* For chat.html */
-
-// TODO: Fetch the list of existing chat messages.
-// POST to the API when the user posts a new message.
-// Automatically poll for new messages on a regular interval.
 function postMessage() {
-  return;
+  let chatId = parseInt(window.location.pathname.substr(6))
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  let sessionToken = urlSearchParams.get('session_token');
+  let chat = document.getElementById("chat");
+  let chatBody = chat.value
+  if (chatBody == "") { return; }
+
+  let api = `/api/messages?chat_id=${chatId}&session_token=${sessionToken}`;
+  let body = JSON.stringify({
+    chat_id: chatId,
+    session_token: sessionToken,
+    message_body: chatBody,
+  });
+  let request = createPostRequest(api, body)
+
+  request.then(function(response) {
+    response.json().then(function(data) {
+      chat.value = ''
+    })
+  })
 }
 
-function getMessages() {
-  return;
+function showMessages(chatId, data, preData) {
+  if (data.length != 0 && (data.length != preData.length || data[0].timestamp != preData[0].timestamp)) {
+    const messagesBox = document.getElementById("messages-box");
+    messagesBox.innerHTML = '';
+    for (i = 0; i < data.length; i++) {
+      let message = data[i]
+      let messageBlock = document.createElement('div');
+      messageBlock.className = 'message'
+      messageBlock.id = `chat${chatId}-msg${i}`
+
+      let msgTitleBlock = document.createElement('p');
+      msgTitleBlock.className = 'title'
+      let usernameSpan = document.createElement('span');
+      usernameSpan.className = 'username'
+      usernameText = document.createTextNode(message.username)
+      usernameSpan.appendChild(usernameText)
+      let timestampSpan = document.createElement('span');
+      timestampSpan.className = 'timestamp'
+      timestampText = document.createTextNode(message.timestamp.substr(11))
+      timestampSpan.appendChild(timestampText)
+      msgTitleBlock.appendChild(usernameSpan)
+      msgTitleBlock.appendChild(timestampSpan)
+      
+      let msgBodyBlock = document.createElement('p');
+      msgBodyBlock.className = 'body'
+      let msgBodyText = document.createTextNode(message.body)
+      msgBodyBlock.appendChild(msgBodyText)
+      
+      messageBlock.appendChild(msgTitleBlock)
+      messageBlock.appendChild(msgBodyBlock)
+
+      messagesBox.append(messageBlock)
+    }
+    window.scrollTo(0,document.body.scrollHeight);
+  }
 }
 
 function startMessagePolling() {
-  return;
+  let chatId = parseInt(window.location.pathname.substr(6))
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  let sessionToken = urlSearchParams.get('session_token');
+  let api = `/api/messages?chat_id=${chatId}&session_token=${sessionToken}`;
+  let preMessages = [];
+  setInterval(function() {
+    let request = createGetRequest(api)
+    request.then(function(response) {
+      response.json().then(function(data) {
+        messages = data.messages;
+        showMessages(chatId, messages, preMessages);
+        preMessages = messages;
+      })
+    })
+  }, MSG_ROLLING_INTERVAL)
 }
+
+function getMagicLink() {
+  let chatId = parseInt(window.location.pathname.substr(6))
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  let sessionToken = urlSearchParams.get('session_token');
+  let api = `/api/link?chat_id=${chatId}&session_token=${sessionToken}`;
+  let request = createGetRequest(api)
+  request.then(function(response) {
+    response.json().then(function(data) {
+      window.alert(data.magic_link)
+    })
+  })
+}
+
+function getUserChats() {
+  let chatId = parseInt(window.location.pathname.substr(6))
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  let sessionToken = urlSearchParams.get('session_token');
+  let api = `/api/chats?chat_id=${chatId}&session_token=${sessionToken}`;
+  let request = createGetRequest(api)
+  request.then(function(response) {
+    response.json().then(function(data) {
+      console.log(data)
+      const userChats = data.chats
+      const chatsUl = document.getElementById("user-chats")
+      for (i = 0; i < userChats.length; i++) {
+        let userChat = userChats[i]
+        let userChatId = userChat.chat_id
+        let userSessionToken = userChat.session_token
+        let chatLi = document.createElement('li')
+        chatLi.innerHTML = `<a href="/chat/${userChatId}?session_token=${userSessionToken}">${userChatId}</a>`
+        if (userChatId == chatId) {
+          chatLi.setAttribute("style", "background: rgb(128, 0, 128)");
+        }
+        chatsUl.append(chatLi)
+        
+      }
+    })
+  })
+} 
